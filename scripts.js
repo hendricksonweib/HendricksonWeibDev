d2cc67a421bfac2d71888df5858c260e7499297c
hendricksonweib <hendricksonrocha22@gmail.com>
2024-05-12 00:13:42 -0300

    feat: session wrapper
    Signed-off-by: hendricksonweib <hendricksonrocha22@gmail.com>

--- scripts.js
+++ scripts.js
@@ -54,3 +54,65 @@ jQuery(document).ready(($) => {
   const target = $('#navbarSupportedContent ul li a[href="' + path + '"]')
   target.parent().addClass("active")
 })
+
+/* MEDIA */
+gsap.from(".media ul li", {
+  duration: 1.5,
+  delay: 1.5,
+  stagger: 0.08,
+  opacity: 0,
+  x: "-20",
+  ease: "expo.inOut",
+});
+
+/* TEXT */
+gsap.from(".text h1 .hide--text", {
+  duration: 1.5,
+  delay: 1,
+  y: "100%",
+  ease: "expo.inOut",
+});
+
+gsap.from(".text h3 .hide--text", {
+  duration: 1.5,
+  delay: 1.2,
+  y: "100%",
+  ease: "expo.inOut",
+});
+
+gsap.from(".text p .hide--text", {
+  duration: 1.5,
+  delay: 1.3,
+  y: "200%",
+  ease: "expo.inOut",
+});
+
+gsap.from(".text h2", {
+  duration: 1.5,
+  delay: 1.5,
+  opacity: 0,
+  x: "-10000",
+  ease: "expo.inOut",
+});
+
+/* OVERLAY */
+gsap.to(".first", {
+  duration: 1.5,
+  delay: 0.5,
+  top: "-100%",
+  ease: "expo.inOut",
+});
+
+gsap.to(".second", {
+  duration: 1.5,
+  delay: 0.6,
+  top: "-100%",
+  ease: "expo.inOut",
+});
+
+gsap.to(".third", {
+  duration: 1.5,
+  delay: 0.7,
+  top: "-100%",
+  ease: "expo.inOut",
+});
